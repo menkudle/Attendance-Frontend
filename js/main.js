223fb7cffc0d2f898b15483f638c449f119cdf8c
menkudle <shubham.menkudle.cse@gmail.com>
2018-03-17 11:25:46 +0530

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+(function($) {
+if ($(window).width() > 20) {
+    var headerHeight = $('.cd-header').height();
+    $(window).on('scroll', {
+            previousTop: 0
+        },
+        function() {
+            var currentTop = $(window).scrollTop();
+            //check if user is scrolling up
+            if (currentTop < this.previousTop) {
+                //if scrolling up...
+                if (currentTop > 0 && $('.cd-header').hasClass('is-fixed')) {
+                    $('.cd-header').addClass('is-visible');
+                } else {
+                    $('.cd-header').removeClass('is-visible is-fixed');
+                }
+            } else {
+                //if scrolling down...
+                $('.cd-header').removeClass('is-visible');
+                if (currentTop > headerHeight && !$('.cd-header').hasClass('is-fixed')) $('.cd-header').addClass('is-fixed');
+            }
+            this.previousTop = currentTop;
+        });
+}
+
+$('.cd-primary-nav-trigger').on('click', function() {
+    $('.cd-menu-icon').toggleClass('is-clicked');
+    $('.cd-header').toggleClass('menu-is-open');
+
+    //in firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
+    if ($('.cd-primary-nav').hasClass('is-visible')) {
+        $('.cd-primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
+            $('body').removeClass('overflow-hidden');
+        });
+    } else {
+        $('.cd-primary-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
+            $('body').addClass('overflow-hidden');
+        });
+    }
+});
+
+
+})(jQuery);
